d79e2892b46395c574332607a83f1938f27b8b1c
jhernandezb <contact@jhernandez.me>
2023-10-20 15:29:16 -0600

    fix query

--- client/query/inventory.ts
+++ client/query/inventory.ts
@@ -3,16 +3,16 @@ import { NFT_API } from "util/constants";
 export default async function queryInventory(address: string) {
   let tokenList = [];
 
-  tokenList = await fetch(`${NFT_API}/api/pegasus/profile/${address}/paginated_nfts`).then(
-    (res) => {
-      return res.json();
-    }
-  );
+  tokenList = await fetch(
+    `${NFT_API}/api/pegasus/profile/${address}/paginated_nfts`
+  ).then((res) => {
+    return res.json();
+  });
 
   // sort by nft name
   tokenList.tokens.sort((a: any, b: any) =>
     a.name.localeCompare(b.name, undefined, { numeric: true })
   );
 
-  return tokenList;
+  return tokenList.tokens;
 }
