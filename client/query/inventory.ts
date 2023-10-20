0dceeaae1ae140fbf16dec0f77de02cb6936e494
Jorge Hernandez <3452489+jhernandezb@users.noreply.github.com>
2023-10-20 15:17:01 -0600

    Update inventory.ts

--- client/query/inventory.ts
+++ client/query/inventory.ts
@@ -3,7 +3,7 @@ import { NFT_API } from "util/constants";
 export default async function queryInventory(address: string) {
   let tokenList = [];
 
-  tokenList = await fetch(`${NFT_API}/api/v1beta/profile/${address}/nfts`).then(
+  tokenList = await fetch(`${NFT_API}/api/pegasus/profile/${address}/paginated_nfts`).then(
     (res) => {
       return res.json();
     }
